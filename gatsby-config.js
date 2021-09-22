666c00d22f6267b819142039de211a14dffb1c75
herby <tomasekerbenu@gmail.com>
2021-09-23 00:17:16 +0200

    deleted whats causing error in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Tomáš Erben portfolio`,
+    description: `Hey I am a self-taught frontend developer. Check out my portfolio.`,
     author: `@gatsbyjs`,
     siteUrl: `https://tomaserben.eu/`,
   },
@@ -18,7 +18,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
